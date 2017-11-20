851335e731764a93351c06e496ce79656d0a0df6
Emad Saber Hamed <omda.sharkawy@gmail.com>
2017-11-20 15:52:54 +0200

    staged entities

---
+++ DAL/Domain/Models/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Domain.Models
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+        public int? ModifiedBy { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+        public DateTime? CreatedDate { get; set; }
+    }
+}
